475142d18acda94ba2cd8d22cdb4c24045a0f030
LeonardoZuin <leonardo.zuin@e-unicred.com.br>
2024-12-16 14:40:19 -0300

    chore: adiciona plugin Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BFF NestJs - APIs Públicas')
+    .setDescription('BFF usando NestJS que conecta 3 APIs públicas para criar endpoints de baixa, média e alta complexidade.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
