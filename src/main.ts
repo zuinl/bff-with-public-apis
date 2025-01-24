1835c9516feaee76fde55ff0b967fa7047cbafca
LeonardoZuin <leonardo.zuin@e-unicred.com.br>
2025-01-24 10:17:04 -0300

    chore: inicia esqueleto do projeto

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BFF com APIs públicas')
+    .setDescription(
+      'BFF construído com NestJS que consome APIs públicas para criar endpoints de variadas complexidades',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(3000);
+}
+void bootstrap();
